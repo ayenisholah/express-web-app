5a9e3d70c244cb66f7cdf01b917bf9d6566373d8
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-18 18:44:36 +0100

    pug html template

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-const app = express();;
+const app = express();
 
 app.use(morgan('tiny'));
 
@@ -14,12 +14,14 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'))
+  res.render('index');
 });
 
 app.listen(3000, () => {
-  debug(`listening on port ${chalk.green('3000')}`)
+  debug(`magic happening ${chalk.green('3000')}`);
 });
