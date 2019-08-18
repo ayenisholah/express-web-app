602b14e3a13291981975370b160f11d0c40ed2b1
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-18 19:21:00 +0100

    ejs template

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'] });
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(3000, () => {
